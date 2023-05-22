229b5017dd88f10a2e53524f22510f7347170096
Autumn <76168014+Autumn64@users.noreply.github.com>
2023-05-21 18:10:29 -0600

    Add files via upload

---
+++ 2023/2nd_semester/suma.cpp
@@ -0,0 +1,18 @@
+//Autor: Aurora Giselle Flores Gomez.
+
+#include<iostream>
+using namespace std;
+
+int main() {
+    int valor1, valor2, suma;
+
+    cout << "Dame el valor uno: ";
+    cin >> valor1;
+    cout << "\nDame el valor dos: ";
+    cin >> valor2;
+    suma = valor1 + valor2;
+    cout << "\nLa suma es:\n";
+    cout << suma;
+
+    return 0;
+}
