c0e2cc1802f35637a3837cc7b7a9eafbb7110eed
Autumn64 <twicebiased20152010@hotmail.com>
2023-09-08 13:37:29 -0600

    2023-09-08

---
+++ 2023/3rd_semester/vehiculo/Automovil.java
@@ -0,0 +1,27 @@
+package vehiculo;
+
+public class Automovil{
+	private String estado, marca, color;
+	private int vMax;
+
+	public Automovil(String estado, String marca, String color, int vMax){
+		this.estado = estado;
+		this.marca = marca;
+		this.color = color;
+		this.vMax = vMax;
+	}
+
+	public Automovil(){
+		this.estado = "Apagado";
+		this.marca = "Volkswagen";
+		this.color = "Azul";
+		this.vMax = 260;
+	}
+	
+	public void obtenerDatos(){
+		System.out.println("Estado: " + estado);
+		System.out.println("Marca: " + marca);
+		System.out.println("Color: " + color);
+		System.out.println("Velocidad máxima: " + vMax + "km/h");
+	}
+}
---
+++ 2023/3rd_semester/vehiculo/Vehiculo.java
@@ -0,0 +1,13 @@
+package vehiculo;
+
+public class Vehiculo{
+	public static void main(String []args){
+		Automovil auto1 = new Automovil("Encendido", "Ferrari", "Rojo", 400);
+		Automovil auto2 = new Automovil();
+
+		System.out.println("Automovil 1:");
+		auto1.obtenerDatos();
+		System.out.println("\nAutomovil 2:");
+		auto2.obtenerDatos();
+	}
+}
