19cebf01a4e8b9bb852982931066ecda5d24e646
Autumn64 <eunbyeol64@naver.com>
2023-12-01 17:44:59 -0600

    2023-12-01

---
+++ 2023/3rd_semester/operacionesgui/Operacionesgui.java
@@ -0,0 +1,11 @@
+//Made by Mónica Gómez (Autumn64)
+
+package operacionesgui;
+public class Operacionesgui {
+
+    public static void main(String[] args) {
+        Main w = new Main();
+        w.setVisible(true);
+    }
+    
+}
